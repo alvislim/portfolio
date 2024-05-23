ca8d4864b8f8d4fc6c4578c5f3c3aad6ea30f2e6
alvislim <luvhatehero1@gmail.com>
2024-05-23 14:05:52 +0800

    update navbar design

--- portfolio/src/component/NavBar/index.tsx
+++ portfolio/src/component/NavBar/index.tsx
@@ -11,7 +11,7 @@ const NavBar = (props: Props) => {
   const { navBarArr } = props;
 
   return (
-    <motion.div
+    <motion.nav
       className='navbar-container'
       initial={{
         opacity: 0,
@@ -25,23 +25,56 @@ const NavBar = (props: Props) => {
         duration: 1,
         ease: [0.6, 0.05, 0.01, 0.9],
       }}>
-      {navBarArr.map((item, indx) => {
-        return (
+      <ul>
+        <li>
           <Link
             className='navbar-element'
             activeClass='active'
-            to={item.id}
+            to={""}
             isDynamic={true}
             spy={true}
             offset={-70}
             duration={1000}
-            smooth={"easeInOutQuint"}
-            key={`${item}_${indx}`}>
-            {item.title}
+            smooth={"easeInOutQuint"}>
+            Download Resume
           </Link>
-        );
-      })}
-    </motion.div>
+        </li>
+        {navBarArr.map((item, indx) => {
+          return (
+            <li>
+              <Link
+                className='navbar-element'
+                activeClass='active'
+                to={item.id}
+                isDynamic={true}
+                spy={true}
+                offset={-70}
+                duration={1000}
+                smooth={"easeInOutQuint"}
+                key={`${item}_${indx}`}>
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+        <li>
+          <input
+            type='checkbox'
+            id='checkbox4'
+            className='checkbox4 visuallyHidden'
+          />
+          <label htmlFor='checkbox4'>
+            <div className='hamburger hamburger4'>
+              <span className='bar bar1'></span>
+              <span className='bar bar2'></span>
+              <span className='bar bar3'></span>
+              <span className='bar bar4'></span>
+              <span className='bar bar5'></span>
+            </div>
+          </label>
+        </li>
+      </ul>
+    </motion.nav>
   );
 };
 
